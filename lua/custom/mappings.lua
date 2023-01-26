5297fc1b48f5d69445f4f9ddb06db4999358fcdc
乔云 <rongqiao.yurq@taobao.com>
2023-01-26 13:53:42 +0800

    update key mappings.

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -9,15 +9,26 @@ M.general = {
   v ={
     ["<leader>fr"] = {"<ESC>:lua require('spectre').open_visual()<CR>", "replace global"},
   },
+  x = {
+    ["<leader>a"] = {"<Plug>(coc-codeaction-selected)", "code acton selcected"},
+  },
   n = {
+    ["<leader>qq"] = {":wqa<CR>", "save and exit"},
+
+    ["tt"] = {":CocCommand explorer<CR>", "open file exploerer"},
+
     ["gd"] = {"<Plug>(coc-definition)", "go to definition", opts=opt},
     ["gy"] = {"<Plug>(coc-type-definition)", "go to definition", opts=opt},
     ["gi"] = {"<Plug>(coc-implementation)", "go to definition", opts=opt},
     ["gr"] = {"<Plug>(coc-references)", "go to definition", opts=opt},
-    ["tt"] = {":CocCommand explorer<CR>", "open file exploerer"},
     ["<leader>ff"] = {":Telescope find_files<CR>", "find files"},
     ["<leader>fg"] = {":Telescope live_grep<CR>", "find global"},
     ["<leader>fr"] = {":lua require('spectre').open_visual({select_word=true})<CR>", "replace global"},
+    ["<leader>rn"] = {"<Plug>(coc-rename)", "rename"},
+    ["<leader>ac"] = {"<Plug>(coc-codeaction)", "apply code action"},
+    ["<leader>qf"] = {"<Plug>(coc-fix-current)", "apply code action"},
+    ["<leader>a"] = {"<Plug>(coc-codeaction-selected)", "code acton selcected"},
+
     ["<leader>rr"] = { function()
         vim.cmd("w")
         if vim.bo.filetype == "python" then
